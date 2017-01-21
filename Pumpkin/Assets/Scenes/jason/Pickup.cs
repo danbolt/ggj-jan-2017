be1ba85a34a9d336540d5e962bffdaed54b10181
Jason Estey <esteyj@yahoo.com>
2017-01-20 21:25:56 -0800

    added simple pickup logic

---
+++ Pumpkin/Assets/Scenes/jason/Pickup.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+	private BoxCollider _Collider;
+
+	private void Start()
+	{
+		_Collider = GetComponent<BoxCollider>();
+	}
+
+	private void OnTriggerEnter()
+	{
+		Debug.Log("triggered");
+	}
+}
