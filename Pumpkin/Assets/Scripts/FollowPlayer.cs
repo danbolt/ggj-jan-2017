e5b37178b64b169290897e92e0d8d02b1ba15946
alonm10 <alonm10@yahoo.com>
2017-01-21 15:53:58 -0800

    Added camera to follow player

---
+++ Pumpkin/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    public GameObject target;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var cameraTransform = this.GetComponent<Camera>().transform;
+        cameraTransform.position = new Vector3(target.transform.position.x, cameraTransform.position.y, cameraTransform.position.z);
+    }
+}
