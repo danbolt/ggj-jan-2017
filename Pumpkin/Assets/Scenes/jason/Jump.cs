1a037adb79d0b1d0a773c276e489a258e7840cec
Jason Estey <esteyj@yahoo.com>
2017-01-20 22:16:59 -0800

    untested jump audio, setting lives on Jump

--- Pumpkin/Assets/Scenes/jason/Jump.cs
+++ Pumpkin/Assets/Scenes/jason/Jump.cs
@@ -1,19 +1,25 @@
 ﻿
+using System;
 using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
 	public Vector3 Force = new Vector3(0f, 1f, 0f);
 	public string Button = "Jump";
+	public AudioClip[] JumpSounds;
 
 	private Rigidbody _Rigidbody;
 	private CapsuleCollider _Collider;
+	private AudioSource _AudioSource;
 	private float ColliderHeight;
+	private System.Random _Random = new System.Random();
+	private int LastUsedSoundIndex = -1;
 
 	private void Start()
 	{
 		_Rigidbody = GetComponent<Rigidbody>();
 		_Collider = GetComponent<CapsuleCollider>();
+		_AudioSource = GetComponent<AudioSource>();
 		ColliderHeight = _Collider.bounds.extents.y;
 	}
 
@@ -22,6 +28,19 @@ public class Jump : MonoBehaviour
 		if (Input.GetButtonDown(Button) && IsGrounded)
 		{
 			_Rigidbody.AddForce(Force, ForceMode.Impulse);
+
+			int soundCount = JumpSounds.Length;
+			if (soundCount > 0)
+			{
+				int randIndex = _Random.Next(0, soundCount);
+				if (randIndex == LastUsedSoundIndex)
+				{
+					randIndex = (randIndex + 1) % soundCount;
+				}
+
+				var sound = JumpSounds[randIndex];
+				_AudioSource.PlayOneShot(sound);
+			}
 		}
 	}
 
