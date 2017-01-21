28b24c75bc7f2427f7799cca0e34262d4fc8c6a3
alonm10 <alonm10@yahoo.com>
2017-01-21 14:41:52 -0800

    Leve design prototype

--- Pumpkin/Assets/Scripts/Balistic.cs
+++ Pumpkin/Assets/Scripts/Balistic.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class Balistic : MonoBehaviour {
 
     public string Direction = "Left";
-    public float forceMultiplier = 1;
+    public float forceMultiplier = 0.00000005f;
+    private Rigidbody body;
+    private int timesApplied = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        body = this.GetComponent<Rigidbody>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Direction == "Left")
+        if (timesApplied < 10)
         {
-            this.GetComponent<Rigidbody>().AddForce(new Vector3(-1, 0, 0) * forceMultiplier, ForceMode.Impulse);
+            if (Direction == "Left")
+            {
+                body.AddForce(new Vector3(-1, 0, 0) * forceMultiplier, ForceMode.VelocityChange);
+            }
+            else if (Direction == "Right")
+            {
+                body.AddForce(new Vector3(1, 0, 0) * forceMultiplier, ForceMode.VelocityChange);
+            }
+            timesApplied++;
+
         }
-        else if (Direction == "Right")
-        {
-            this.GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0) * forceMultiplier, ForceMode.Impulse);
-        }
+       
     }
 }
