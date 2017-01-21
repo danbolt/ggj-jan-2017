db0aa8302015648797ca36ef65e26653c3239155
Jason Estey <esteyj@yahoo.com>
2017-01-21 11:46:32 -0800

    wave hit detection

---
+++ Pumpkin/Assets/Scenes/jason/WaveHitDetection.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class WaveHitDetection : MonoBehaviour
+{
+	public GameObject Player;
+
+	private void OnTriggerEnter()
+	{
+		if (Player)
+		{
+			Player.SendMessage("HitByWave", null, SendMessageOptions.RequireReceiver);
+		}
+		else
+		{
+			Debug.Log("wave prefab has no player set");
+		}
+	}
+}
