7ed6d21f763b028699ebb5c79360f9444a4d771a
Luis Placid <lplacid@me.com>
2017-01-21 21:46:45 -0800

    Hooking up unpause and the title.

---
+++ Pumpkin/Assets/Source/UI/Screens/HUDManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDManager : MonoBehaviour {
+
+	[SerializeField]
+	private UILabel scoreLabel;
+
+	private GameManager gameManager;
+	private GuiManager guiManager;
+
+	// Use this for initialization
+	void Start () {
+		this.gameManager = GameManager.Instance;
+		this.guiManager = GuiManager.Instance;
+		UpdateScoreLabel();
+	}
+
+	void UpdateScoreLabel()
+	{
+		if (this.gameManager != null) 
+		{
+			scoreLabel.text = this.gameManager.CurrentScore.ToString();
+		}	
+	}
+
+	void Update()
+	{
+		UpdateScoreLabel();
+	}
+
+	public void PauseGame() {
+		if (this.guiManager != null)
+		{
+			this.guiManager.TriggerGameplayPause();
+		}
+//		this.gameManager.Pause();
+	}
+}
---
+++ Pumpkin/Assets/Source/UI/Screens/PauseScreenManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScreenManager : MonoBehaviour {
+
+	private GuiManager guiManager;
+
+	public void ResumeGameplay()
+	{
+		if (guiManager != null) 
+		{
+			this.guiManager.TriggerGameplayResume();
+		}
+	}
+
+	private void Start()
+	{
+		this.guiManager = GuiManager.Instance;
+	}
+
+}
