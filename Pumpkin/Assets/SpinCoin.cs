426b7a82c1a802f81651495e1837212f187c0372
Jason Estey <esteyj@yahoo.com>
2017-01-21 20:38:19 -0800

    spin coin script cleanup

--- Pumpkin/Assets/SpinCoin.cs
+++ Pumpkin/Assets/SpinCoin.cs
@@ -1,24 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
-public class SpinCoin : MonoBehaviour {
+public class SpinCoin : MonoBehaviour
+{
+	public float TimeScale = 1.0f;
 
-    private Transform parentObjectTransform;
+	private Transform _Transform;
 
-    public float TimeScale = 1.0f;
-
-	/// <summary>
-    /// Called once when the frame begins.
-    /// </summary>
-	void Start () {
-        parentObjectTransform = GetComponent<Transform>();
+	void Start()
+	{
+		_Transform = GetComponent<Transform>();
 	}
-	
-	/// <summary>
-    /// Called once per physics update (zero or more times per frame).
-    /// </summary>
-	void FixedUpdate () {
-        parentObjectTransform.Rotate(0.0f, Time.deltaTime * TimeScale, 0.0f);
+
+	void FixedUpdate()
+	{
+		_Transform.Rotate(0.0f, Time.deltaTime * TimeScale, 0.0f);
 	}
 }
