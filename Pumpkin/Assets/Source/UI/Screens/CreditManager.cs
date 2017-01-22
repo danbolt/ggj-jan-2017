172c4e15d0621d198198e19cd79df04fa81c02ad
Luis Placid <lplacid@me.com>
2017-01-22 12:29:53 -0800

    We have Popups now.

---
+++ Pumpkin/Assets/Source/UI/Screens/CreditManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditManager : MonoBehaviour {
+
+	public void GoBackToMainMenu()
+	{
+		if (GuiManager.Instance)
+		{
+			GuiManager.Instance.ReturnToMainMenu();
+		}	
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
