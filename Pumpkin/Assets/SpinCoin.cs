c40985685dec63460cc69745cc284c5b553576c4
Daniel Savage <dansvg@gmail.com>
2017-01-21 16:32:29 -0800

    Spin coins with mesh

---
+++ Pumpkin/Assets/SpinCoin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinCoin : MonoBehaviour {
+
+    private Transform parentObjectTransform;
+
+    public float TimeScale = 1.0f;
+
+	/// <summary>
+    /// Called once when the frame begins.
+    /// </summary>
+	void Start () {
+        parentObjectTransform = GetComponent<Transform>();
+	}
+	
+	/// <summary>
+    /// Called once per physics update (zero or more times per frame).
+    /// </summary>
+	void FixedUpdate () {
+        parentObjectTransform.Rotate(0.0f, Time.deltaTime * TimeScale, 0.0f);
+	}
+}
